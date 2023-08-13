e09888b25e1189ff3f6808c4b9f04db3f6094176
Nadina Oates <nadinaoates@gmail.com>
2023-08-13 15:14:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Encode Bootcamp Week 2 - Homework
+
+Team members: Nadina
+
+## Nadina's Contract:
+[https://sepolia.etherscan.io/address/0x24d810964c578a9d543618e59ce5b96dc82323d2](https://sepolia.etherscan.io/address/0xa78208769cdE9A2921Fd9D227EfBbe0f5F429Eb0)
+
+**Deployment** by 0x7Bb8be3D9015682d7AC0Ea377dC0c92B0ba152eF:
+[https://sepolia.etherscan.io/tx/0x6c614972361c2f7da0a714790c4a48f496cce7433ab8fdfdcdb992209e11c756](https://sepolia.etherscan.io/tx/0x195cbd8e6973f92314a672bc5aecc0225b5d44cfe4d886e4f04c672031694eaa)
+
+**GiveRightToVote to 3 addresses**:<br />
+-> transactions successful<br />
+[https://sepolia.etherscan.io/tx/0x3746ed94fbb716719fde731fb5d201881bfcda430e2a6a109e871154ef3eebf7](https://sepolia.etherscan.io/tx/0x26058813237c6450f1b61cc789f0bd02081e1c46932558342f90242259b48cca)<br />
+https://sepolia.etherscan.io/tx/0x90ec79a5b431356bff6e342277242c2a7ba4254d290571e9889e03a4ad09c6dd<br />
+https://sepolia.etherscan.io/tx/0x1934c28357195cebc70cbc29a55c80b6bdd13158b0ced0207d0366d807268aea
+
+**Delegate from 0x9db400AeABed9005f3a1D32Bf1e132F44E53dCfa to 0x582b25a263c46004B0A476A78ECFf7aE7E2034a0**:<br />
+-> successful<br />
+https://sepolia.etherscan.io/tx/0xb6f4189ea8af348b51a2fed4499bd63d5b05152dc0da53892237e857bc4d9fa0
+
+**Voting by 0x582b25a263c46004B0A476A78ECFf7aE7E2034a0 and 0x6652c1F62F8a1907f4F2a9b5f557ec62e7978050**:<br />
+-> successful<br />
+https://sepolia.etherscan.io/tx/0xf91f1df4d0d740113a5744aed2eab80a50f01dc98e363395bfd8764eaa457c18<br />
+https://sepolia.etherscan.io/tx/0x55f47ea00afea9d54445304f12bd29a96810100ca67eb5e47909aee223c4072e
+
+**Query Results**:<br />
+Winning propsal: 1<br />
+Winner name: chocolate
+
---
+++ scripts/ERC20Votes.ts
@@ -0,0 +1,76 @@
+import {ethers} from "hardhat";
+import {MyToken__factory} from "../typechain-types";
+
+const MINT_VALUE = ethers.parseUnits("1");
+async function main() {
+    const [deployer, acc1, acc2] = await ethers.getSigners();
+    const contractFactory = new MyToken__factory(deployer);
+    const contract = await contractFactory.deploy();
+    await contract.waitForDeployment();
+    const contractAddress = await contract.getAddress();
+    console.log(`Token contract deployed at ${ contractAddress }`);
+
+    // Mint some tokens
+    const mintTx = await contract.mint(acc1.address, MINT_VALUE);
+    await mintTx.wait();
+    console.log(
+        `Minted ${ MINT_VALUE.toString() } decimal units to account ${ acc1.address }\n`
+    );
+    const balanceBN = await contract.balanceOf(acc1.address);
+    console.log(
+        `Account ${ acc1.address
+        } has ${ balanceBN.toString() } decimal units of MyToken\n`
+    );
+    console.log(
+        `Account ${ acc1.address
+        } has ${ ethers.formatUnits(balanceBN) } units of MyToken\n`
+    );
+
+    // Self delegate
+    const delegateTx = await contract.connect(acc1).delegate(acc1.address);
+    await delegateTx.wait();
+
+    // Check the voting power
+    const votes = await contract.getVotes(acc1.address);
+    console.log(
+        `Account ${ acc1.address } has ${ votes.toString() } units of voting power`
+    );
+
+    // Transfer tokens
+    const transferTx = await contract
+        .connect(acc1)
+        .transfer(acc2.address, MINT_VALUE / 2n);
+    await transferTx.wait();
+
+    // Check the voting power
+    const votes1AfterTransfer = await contract.getVotes(acc1.address);
+    console.log(
+        `Account ${ acc1.address
+        } has ${ votes1AfterTransfer.toString() } units of voting power after transferring\n`
+    );
+    const votes2AfterTransfer = await contract.getVotes(acc2.address);
+    console.log(
+        `Account ${ acc2.address
+        } has ${ votes2AfterTransfer.toString() } units of voting power after receiving a transfer\n`
+    );
+
+    // Check past voting power
+    const lastBlock = await ethers.provider.getBlock("latest");
+    const lastBlockNumber = lastBlock?.number ?? 0;
+
+    for (let index = lastBlockNumber - 1; index > 0; index--) {
+        const pastVotes = await contract.getPastVotes(
+            acc1.address,
+            index
+        );
+        console.log(
+            `Account ${ acc1.address
+            } had ${ pastVotes.toString() } units of voting power at block ${ index }\n`
+        );
+    }
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
